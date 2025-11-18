81b61bee62dff2330516117d7e1b2325f702d0b1
madhukayal88-glitch <madhukayal88@gmail.com>
2025-11-18 11:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+function daysOfAYear(year) {
+  // Leap year logic:
+  // 1. Must be divisible by 4
+  // 2. If divisible by 100, must also be divisible by 400
+  const isLeap =
+    (year % 4 === 0 && year % 100 !== 0) ||
+    (year % 400 === 0);
+
+  return isLeap ? 366 : 365;
+}
+
